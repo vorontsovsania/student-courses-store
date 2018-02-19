2b00af8d07f131e3d88f10a6b71d58524049dbc2
Александр <vorontsovsania@gmail.com>
2018-02-19 09:24:05 +0300

    Add Students db table

---
+++ CoursesStore.Database/dbo/Tables/Students.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [StudentId] INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [LastName]  NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([StudentId] ASC)
+);
+
