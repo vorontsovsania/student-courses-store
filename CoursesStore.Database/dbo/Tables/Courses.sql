2f2a81185ca76c999745e97aa793f4aac72a3c85
Александр <vorontsovsania@gmail.com>
2018-06-28 16:23:48 +0300

    Update db schema (add directions, courses tables)

---
+++ CoursesStore.Database/dbo/Tables/Courses.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Courses] (
+    [CourseId]          INT            IDENTITY (1, 1) NOT NULL,
+    [Title]             NVARCHAR (100) NOT NULL,
+    [CourseDirectionId] INT            NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED ([CourseId] ASC),
+    CONSTRAINT [FK_Courses_CourseDirections] FOREIGN KEY ([CourseDirectionId]) REFERENCES [dbo].[CourseDirections] ([CourseDirectionId])
+);
+
