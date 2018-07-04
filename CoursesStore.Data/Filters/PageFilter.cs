5830f9cb9a2b8563a706ab005570b52c68b8ee80
Александр <vorontsovsania@gmail.com>
2018-07-04 16:40:07 +0300

    Implement GetPagedStudentList repository method

---
+++ CoursesStore.Data/Filters/PageFilter.cs
@@ -0,0 +1,10 @@
+﻿using CoursesStore.Common.Paging;
+
+namespace CoursesStore.Data.Filters
+{
+    public class PageFilter<T>
+    {
+	    public T Filter { get; set; }
+	    public Pager Pager { get; set; }
+    }
+}
